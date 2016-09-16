d723fe18db4c52533259bc8838dcdbe1728fcb9a
Ruben Sternke <ruben@sternke.dk>
2016-09-16 13:39:15 +0200

    Updated.

--- BlinkRTOS/BlinkRTOS.ino
+++ BlinkRTOS/BlinkRTOS.ino
@@ -18,18 +18,22 @@
 
 // define one task for Blink 
 void TaskBlinkYellow( void *pvParameters );
+void TaskBlinkGreen( void *pvParameters );
 
 // The setup function runs once when you press reset or power the board
 void setup() {
+  unsigned short timeout = 200;
 
-  // Now set up one task to run independently.
+  // Now set up one task to run independently.  
   xTaskCreate(
-    TaskBlinkYellow
+    TaskBlinkYellow                       // The task function
     ,  (const portCHAR *)"Blink Yellow"   // A name just for humans
-    ,  64  // Stack size
-    ,  NULL
-    ,  2  // priority
-    ,  NULL );
+    ,  64                                 // Stack size (buswidth x size)
+    ,  (void*)&timeout                    // Arguments to the task
+    ,  2                                  // priority     
+    ,  NULL );                            // ??
+
+  xTaskCreate(TaskBlinkGreen, (const portCHAR *)"Blink Green", 64, NULL, 2, NULL ); 
 
   // Now the task scheduler, which takes over control of scheduling individual tasks, is automatically started.
 }
@@ -42,20 +46,37 @@ void loop()
 /*--------------------------------------------------*/
 /*---------------------- Tasks ---------------------*/
 /*--------------------------------------------------*/
-
+// The thread implementation for the yellow LED
 void TaskBlinkYellow(void *pvParameters)  // This is a task.
 {
   (void) pvParameters;
-
-  // initialize digital pin 13 as an output.
+  unsigned short timeout = (unsigned short) (*pvParameters);
+  // Initialize digital pin with the yellow LED as an output.
   pinMode(PIN_YELLOW, OUTPUT);
 
   for (;;) // A Task shall never return or exit.
   {
     digitalWrite(PIN_YELLOW, HIGH);   // turn the LED on (HIGH is the voltage level)
-    vTaskDelay( 1000 / portTICK_PERIOD_MS ); // wait for one second
+    vTaskDelay( timeout / portTICK_PERIOD_MS ); // wait for one second
     digitalWrite(PIN_YELLOW, LOW);    // turn the LED off by making the voltage LOW
-    vTaskDelay( 1000 / portTICK_PERIOD_MS ); // wait for one second
+    vTaskDelay( timeout / portTICK_PERIOD_MS ); // wait for one second
+  }
+}
+
+// The thread implementation for the Green LED
+void TaskBlinkGreen(void *pvParameters)  // This is a task.
+{
+  (void) pvParameters;
+
+  // initialize digital pin with the green LED as an output.
+  pinMode(PIN_GREEN, OUTPUT);
+
+  for (;;) // A Task shall never return or exit.
+  {
+    digitalWrite(PIN_GREEN, HIGH);   // turn the LED on (HIGH is the voltage level)
+    vTaskDelay( 500 / portTICK_PERIOD_MS ); // wait for 0.5 seconds
+    digitalWrite(PIN_GREEN, LOW);    // turn the LED off by making the voltage LOW
+    vTaskDelay( 500 / portTICK_PERIOD_MS ); // wait for 0.5 second
   }
 }
 
